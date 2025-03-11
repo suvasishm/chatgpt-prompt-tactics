962ed7dd0add8478a6d18453663c09209819aa1d
Suvasish Mondal <Suvasish.Mondal@transformco.com>
2025-03-11 15:36:45 -0500

    principal 2

--- prompts.py
+++ prompts.py
@@ -144,7 +144,7 @@ print("Completion for Tactic 5/2:")
 print(get_completion(prompt))
 
 # Tactic 6: Instruct the model to work out its own solution before rushing to a conclusion¶
-```python
+````python
 prompt = f"""
 Your task is to determine if the student's solution \
 is correct or not.
@@ -157,48 +157,48 @@ you have done the problem yourself.
 
 Use the following format:
 Question:
-\```
+```
 question here
-\```
+```
 Student's solution:
-\```
+```
 student's solution here
-\```
+```
 Actual solution:
-\```
+```
 steps to work out the solution and your solution here
-\```
-Is the student's solution the same as actual solution \\
+```
+Is the student's solution the same as actual solution \
 just calculated:
-\```
+```
 yes or no
-\```
+```
 Student grade:
-\```
+```
 correct or incorrect
-\```
+```
 
 Question:
-\```
-I'm building a solar power installation and I need help \\
+```
+I'm building a solar power installation and I need help \
 working out the financials. 
 - Land costs $100 / square foot
 - I can buy solar panels for $250 / square foot
-- I negotiated a contract for maintenance that will cost \\
-me a flat $100k per year, and an additional $10 / square \\
+- I negotiated a contract for maintenance that will cost \
+me a flat $100k per year, and an additional $10 / square \
 foot
-What is the total cost for the first year of operations \\
+What is the total cost for the first year of operations \
 as a function of the number of square feet.
-\``` 
+``` 
 Student's solution:
-\```
+```
 Let x be the size of the installation in square feet.
 Costs:
 1. Land cost: 100x
 2. Solar panel cost: 250x
 3. Maintenance cost: 100,000 + 100x
 Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
-\```
+```
 Actual solution:
 """
 print("Completion for Tactic 6:")
